d21bf4752c996edc1c5adaf80f2e1bdd23971fb3
Adrain <silent1119@163.com>
2024-03-23 20:49:03 +0800

    add email

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,13 @@ const IndexPage = () => {
     <main>
       <h1>Welcome to warmwarm daily!</h1>
       
-      <h1>Coming soon</h1>
+      <body>
+        <p>
+          I have no ideas. I need like-minded netizens. You may contact me.
+          {/* email silent1119@163.com */}
+          <a href="mailto:silent1119@163.com">silent1119@163.com</a>
+        </p>
+      </body>
     </main>
   )
 }
